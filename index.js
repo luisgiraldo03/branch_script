b3be54f732195d14b8ace0f795e18a57ef713c13
Luis Giraldo <lgiraldo@deloitte.com>
2022-12-16 17:48:03 -0500

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const shell =  require('shelljs');
 
 
 shell.ls().forEach(folder => {
+    shell.exec('git branch');
     shell.cd(folder);
     shell.exec('git checkout ', process.argv[2]);
     shell.cd('..')
